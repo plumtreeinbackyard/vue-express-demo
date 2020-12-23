7bd3204953db3f7cfb0f38cbe89d4bbf10c1a769
Janet <plumtreeinbackyard@gmail.com>
2020-12-23 12:19:06 -0700

    Revert "add files for deploying on aws ec2"
    This reverts commit 151c2766c9096fb91ca3773c30cd63a6ba127723.

--- aws-client.js
+++
@@ -1,5 +0,0 @@
-const AWS = require('aws-sdk');
-AWS.config.update({ region: 'us-west-1' });
-const ssm = new AWS.SSM();
-
-module.exports = ssm;
--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,5 @@
 const monk = require('monk');
-const secrets = require('../secrets');
-var connectionString = 'localhost/vueExpressDemo';
-
-if (process.env.NODE_ENV == 'production') {
-  connectionString = secrets
-    .getSecret('/vue-express-demo/mongodburi')
-    .then((result) => {
-      console.log(`mongodb uri: ${result}`);
-    });
-} else {
-  console.log(`Local dev environment.`);
-}
-
+const connectionString = process.env.MONGODB_URI || 'localhost/vueExpressDemo';
 const db = monk(connectionString);
-
+ 
 module.exports = db;
