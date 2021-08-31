24829f6cf3278510801b111d36d242fd6054e804
plumtree <38846407+plumtreeinbackyard@users.noreply.github.com>
2021-08-30 21:43:59 -0600

    fix static folder path again

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/products', (req, res) => {
   products.getAll().then((products) => {
